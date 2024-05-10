b4e42088614beb377315ca07575ecf3f2be002b4
Alexey <belvl.alexey@gmail.com>
2024-05-10 12:40:22 +0300

    +search,+logout,+icons,+posts

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 def register(request):
     if request.method == 'POST':
